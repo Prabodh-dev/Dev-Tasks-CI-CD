52f81244c2e93a739f5fba2161f56b8fc2b8703d
Prabodh Raj U R <prabodhrajmow@gmail.com>
2025-07-09 20:42:58 +0530

    Authentication process added

---
+++ server.js
@@ -0,0 +1,16 @@
+import express from "express";
+import dotenv from "dotenv";
+import connectDB from "./config/db.js";
+import authRoutes from "./routes/authRoutes.js";
+
+dotenv.config();
+const app = express();
+connectDB();
+
+app.use(express.json());
+app.use("/api/auth", authRoutes);
+
+const PORT = process.env.PORT;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
